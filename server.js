ebdedd5385695e5b4e901f1e5ab881b0712f6dbb
Anurag Patel <mail2anuragpatel@gmail.com>
2017-11-16 09:00:28 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+
+app.use((req, res, next)=>{
+  var now= new Date().toString();
+  var log = `${now} : ${req.method} ${req.url}`;
+  fs.appendFile('server.log',log + '\n',(err)=>{
+    if(err){
+      console.log(err);
+    }
+  });
+  console.log(log);
+  next();
+});
+
+app.use((req, res , next)=>{
+  res.render('maintanence.hbs');
+});
+app.use(express.static(__dirname+'/public'));
+
+hbs.registerHelper('getCurrentYear', ()=>{
+  return new Date().getFullYear()
+});
+hbs.registerHelper('screamIt',(text)=>{
+  return text.toUpperCase();
+});
+app.set('viewing engine', 'hbs');
+app.get('/', (req, res)=> {
+  res.render('home.hbs',{
+    name : 'anurag',
+    welcome : 'hello!!! welcome to out home page'
+  });
+});
+
+app.get('/about',(req, res)=>{
+  res.render('about.hbs',{
+    PageTitle : 'About Page is here',
+    Content : 'hello there this is me'
+  });
+});
+
+
+
+app.get('/bad', (req, res)=>{
+  res.send('bad request');
+});
+
+app.listen(3000, ()=>{
+  console.log('server is up at port 3000');
+});
